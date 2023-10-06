19b1fd0f487f4bcf25dc4a7bb743e6d385e93fe3
fintan-gallagher <N00222791@iadt.ie>
2023-10-06 10:10:37 +0100

    Amended Contact Page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class ="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to your Laravel Home</div>
+
+                <div class="card-body"><p>This is a contact page for your Laravel Project. You can customize it as per your requirements</p></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
